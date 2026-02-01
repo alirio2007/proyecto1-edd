38971cef7d36da7e0abeca85ee40dde177f8795f
Gian21072006 <gianfranco.camporeale@correo.unimet.edu.ve>
2026-02-01 11:49:36 -0400

    clase deLista simple

---
+++ BioGraph/src/biograph/NodoLista.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package biograph;
+
+/**
+ *
+ * @author 
+ */
+public class NodoLista {
+    public String dato;
+    public NodoLista siguiente;
+    
+    
+    /**
+     * Constructor para crear un nuevo nodo.
+     * 
+     * @param x dato
+     * @param s Siguiente nodo.
+     */
+    public NodoLista(String x, NodoLista s) {
+        dato = x;
+        siguiente = s;
+    }
+}
