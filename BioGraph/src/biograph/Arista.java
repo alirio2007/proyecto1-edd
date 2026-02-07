bed9dda8d6388afe91d85d57d5af24035f2cc8d1
Gian21072006 <gianfranco.camporeale@correo.unimet.edu.ve>
2026-02-07 10:22:21 -0400

    Creacion de arista

---
+++ BioGraph/src/biograph/Arista.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package biograph;
+
+/**
+ *
+ * @author 
+ */
+public class Arista {
+    private int destino;
+    private int peso;
+    
+    public Arista(int destino, int peso) {
+        this.destino = destino;
+        this.peso = peso;
+    }
+    
+    public int getDestino() {
+        return destino;
+    }
+    
+    public int getPeso() {
+        return peso;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Arista arista = (Arista) obj;
+        return destino == arista.destino;
+    }
+}
